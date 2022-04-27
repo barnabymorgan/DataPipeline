3f46b57b0f2c1754199a825212fbb08cb969c5e7
Barnaby Morgan <barnabymorgan@192.168.1.204>
2022-04-27 20:05:38 +0100

    etl airflow

--- DAGS/etl.py
+++ DAGS/etl.py
@@ -1,6 +1,40 @@
+from airflow.models import DAG
+import os
+from os.path import expanduser
+from pathlib import Path
 from datetime import datetime, timedelta
-from textwrap import dedent
+from airflow.operators.bash_operator import BashOperator
 
-# The DAG object; we'll need this to instantiate a DAG
-from airflow import DAG
+home = expanduser("~")
+airflow_dir = os.path.join(home, 'airflow')
+Path(f"{airflow_dir}/dags").mkdir(parents=True, exist_ok=True)
 
+default_args = {
+    'owner': 'Barney',
+    'depends_on_past': False,
+    'email': ['barnabygmorgan@outlook.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'start_date': datetime(2020, 1, 1),
+    'retry_delay': timedelta(minutes=5),
+    'end_date': datetime(2022, 1, 1),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'wait_for_downstream': False,
+    # 'dag': dag,
+    # 'trigger_rule': 'all_success'
+}
+
+with DAG(dag_id='test_dag',
+         default_args=default_args,
+         schedule_interval='*/1 * * * *',
+         catchup=False,
+         tags=['test']
+         ) as dag:
+    # Define the tasks. Here we are going to define only one bash operator
+    test_task = BashOperator(
+        task_id='write_date_file',
+        bash_command='cd ~/Desktop && date >> ai_core.txt',
+        dag=dag)
--- User/streaming_consumer.py
+++ User/streaming_consumer.py
@@ -1,1 +1,32 @@
 from kafka import KafkaConsumer
+import findspark
+
+findspark.init()
+
+import multiprocessing
+
+import pyspark
+
+# We should always start with session in order to obtain
+# context and session if needed
+session = pyspark.sql.SparkSession.builder.config(
+    conf=pyspark.SparkConf()
+    .setMaster(f"local[{multiprocessing.cpu_count()}]")
+    .setAppName("TestApp")
+).getOrCreate()
+
+print(session)
+
+from pyspark.streaming import StreamingContext
+
+# This context can be used with PySpark streaming
+# You might have to specify batchDuration (e.g. on which time window operation will be run)
+# By default data is collected every 0.5 seconds
+ssc = StreamingContext(session.sparkContext, batchDuration=30)
+
+# We will send lines of data to this socketTextStream
+lines = ssc.socketTextStream("localhost", 9999)
+
+unique_words = lines.flatMap(lambda text: text.split()).countByValue()
+
+unique_words.pprint()
